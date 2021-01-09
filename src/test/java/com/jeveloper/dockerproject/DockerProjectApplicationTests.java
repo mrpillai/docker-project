dd423a96536fbb90134ad9ce66ea68b3eb80c25b
mrpillai <pillaimridu@gmail.com>
2021-01-09 21:32:40 +0530

    frst

---
+++ src/test/java/com/jeveloper/dockerproject/DockerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeveloper.dockerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
